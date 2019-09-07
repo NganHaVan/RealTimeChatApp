d6c334d219d840f95f833ba70c99ef4c660b0954
Janet Van <janet@bankify.com.com>
2019-09-08 00:32:02 +0300

    event acknowledgement

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,7 +5,12 @@ document.getElementById("message-form").addEventListener("submit", e => {
   e.preventDefault();
   const messInput = document.querySelector("input").value;
   // console.log(document.getElementsByTagName("input")[0].value);
-  socket.emit("sendMessage", messInput);
+  socket.emit("sendMessage", messInput, messageState => {
+    if (messageState) {
+      console.log("Cursed Words are not allowed");
+    }
+    console.log("Message delivered");
+  });
 });
 
 document.getElementById("send-location").addEventListener("click", () => {
@@ -14,9 +19,15 @@ document.getElementById("send-location").addEventListener("click", () => {
   }
   navigator.geolocation.getCurrentPosition(position => {
     // console.log(position);
-    socket.emit("sendLocation", {
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude
-    });
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      },
+      () => {
+        console.log("Location shared");
+      }
+    );
   });
 });
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const path = require("path");
+const Filter = require("bad-words");
 
 const port = process.env.PORT || 3000;
 // WARNING: app.listen(80) will NOT work here!
@@ -18,17 +19,27 @@ app.use(express.static(publicDirectory));
 io.on("connection", function(socket) {
   socket.emit("message", "Welcome to Chat App");
   socket.broadcast.emit("message", "A new user has joined");
+
   // Client(emit) => Server(receive): "sendMessage": send message to all connected clients
-  socket.on("sendMessage", message => {
+  // NOTE: emiter(a cb as third param) -> receiver(value, cb: the cb must be invoked so that the emiter can receive the message) -- acknowledgement --> emiter
+  // Cb with params ? Not allow cursed words : Display message
+  socket.on("sendMessage", (message, cb) => {
+    const filter = new Filter();
+    if (filter.isProfane(message)) {
+      return cb("Profanity is not allowed");
+    }
     io.emit("message", message);
+    cb();
   });
 
   // Client(emit) => Server(receive): "sendLocation"
-  socket.on("sendLocation", location => {
+  // NOTE: emiter(a cb as third param) -> receiver(value, cb: the cb must be invoked so that the emiter can receive the message) -- acknowledgement --> emiter
+  socket.on("sendLocation", (location, cb) => {
     io.emit(
       "message",
       `Location: https://www.google.com/maps?q=${location.latitude},${location.longitude}`
     );
+    cb();
   });
 
   socket.on("disconnect", () => {
