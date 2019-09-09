71dd2e9366ff059791b935e95434017ed487d49a
Janet Van <janet@bankify.com.com>
2019-09-09 14:31:46 +0300

    UI for chat room and Join Page

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,52 @@
 const socket = io();
 
-socket.on("message", message => console.log(message));
-document.getElementById("message-form").addEventListener("submit", e => {
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = document.querySelector("input");
+const $messageFormButton = document.querySelector("button");
+const $locationButton = document.querySelector("#send-location");
+const $messages = document.getElementById("messages");
+
+// Template
+const messageTemplate = document.getElementById("message-template").innerHTML;
+const locationMessageTemplate = document.getElementById(
+  "location-message-template"
+).innerHTML;
+console.log(document.getElementById("location-message-template"));
+
+// Options
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true
+});
+
+socket.on("message", message => {
+  console.log(message);
+  const html = Mustache.render(messageTemplate, {
+    message: message.text,
+    createdAt: moment(message.createdAt).format("HH:mm")
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
+
+socket.on("locationMessage", locationURL => {
+  // const html = Mustache.render(locationMessageTemplate, { locationURL });
+  const html = Mustache.render(locationMessageTemplate, {
+    locationURL: locationURL.text,
+    createdAt: moment(locationURL.createdAt).format("HH:mm")
+  });
+  $messages.insertAdjacentHTML("beforeend", html);
+});
+
+$messageForm.addEventListener("submit", e => {
   e.preventDefault();
-  const messInput = document.querySelector("input").value;
   // console.log(document.getElementsByTagName("input")[0].value);
+  $messageFormButton.setAttribute("disabled", true);
+  $messageFormInput.setAttribute("disabled", true);
+  const messInput = document.querySelector("input[name=message]").value;
   socket.emit("sendMessage", messInput, messageState => {
+    $messageFormInput.value = "";
+    $messageFormInput.removeAttribute("disabled");
+    $messageFormInput.focus();
+    $messageFormButton.removeAttribute("disabled");
     if (messageState) {
       console.log("Cursed Words are not allowed");
     }
@@ -13,7 +54,8 @@ document.getElementById("message-form").addEventListener("submit", e => {
   });
 });
 
-document.getElementById("send-location").addEventListener("click", () => {
+$locationButton.addEventListener("click", () => {
+  $locationButton.setAttribute("disabled", true);
   if (!navigator.geolocation) {
     return alert("Geolocation is not supported by your browser");
   }
@@ -27,7 +69,10 @@ document.getElementById("send-location").addEventListener("click", () => {
       },
       () => {
         console.log("Location shared");
+        $locationButton.removeAttribute("disabled");
       }
     );
   });
 });
+
+socket.emit("join", { username, room });
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const path = require("path");
 const Filter = require("bad-words");
+const { generateMessage } = require("./utils/messages");
 
 const port = process.env.PORT || 3000;
 // WARNING: app.listen(80) will NOT work here!
@@ -17,8 +18,13 @@ app.use(express.static(publicDirectory));
 // server (emit) => client (receive): "message"->welcome new user
 
 io.on("connection", function(socket) {
-  socket.emit("message", "Welcome to Chat App");
-  socket.broadcast.emit("message", "A new user has joined");
+  socket.on("join", ({ username, room }) => {
+    socket.join(room);
+    socket.emit("message", generateMessage("Welcome to Chat App"));
+    socket.broadcast
+      .to(room)
+      .emit("message", generateMessage(`${username} has joined`));
+  });
 
   // Client(emit) => Server(receive): "sendMessage": send message to all connected clients
   // NOTE: emiter(a cb as third param) -> receiver(value, cb: the cb must be invoked so that the emiter can receive the message) -- acknowledgement --> emiter
@@ -28,7 +34,7 @@ io.on("connection", function(socket) {
     if (filter.isProfane(message)) {
       return cb("Profanity is not allowed");
     }
-    io.emit("message", message);
+    io.emit("message", generateMessage(message));
     cb();
   });
 
@@ -36,13 +42,15 @@ io.on("connection", function(socket) {
   // NOTE: emiter(a cb as third param) -> receiver(value, cb: the cb must be invoked so that the emiter can receive the message) -- acknowledgement --> emiter
   socket.on("sendLocation", (location, cb) => {
     io.emit(
-      "message",
-      `Location: https://www.google.com/maps?q=${location.latitude},${location.longitude}`
+      "locationMessage",
+      generateMessage(
+        `https://www.google.com/maps?q=${location.latitude},${location.longitude}`
+      )
     );
     cb();
   });
 
   socket.on("disconnect", () => {
-    io.emit("message", "A user has left");
+    io.emit("message", generateMessage("A user has left"));
   });
 });
